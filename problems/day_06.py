f0d22af8dab54eef3fe9c178ff7995788afb22a2
Chris Tynan <ctynan@gmail.com>
2022-12-06 11:00:30 +0000

    Day six complete

---
+++ problems/day_06.py
@@ -0,0 +1,29 @@
+INPUT_PREFIX = "c:/Users/ctyna/code/advent_of_code_2022/problems/inputs"
+INPUT_FILE = "day_06.txt"
+input_path = f"{INPUT_PREFIX}/{INPUT_FILE}"
+
+### PART ONE
+f_in = open(input_path, 'r')
+input = next(f_in)
+
+for idx in range(3, len(input)):
+    chars = set()
+    for i in range(4):
+        chars.add(input[idx-i])
+    if len(chars) == 4:
+        print(idx+1)
+        break
+
+### PART TWO
+f_in = open(input_path, 'r')
+input = next(f_in)
+
+for idx in range(13, len(input)):
+    chars = set()
+    for i in range(14):
+        chars.add(input[idx-i])
+    if len(chars) == 14:
+        print(idx+1)
+        break
+
+
